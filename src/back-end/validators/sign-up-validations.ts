452677359f2c31108447b2b5a7711c1f21ba530e
MikeWill07 <Mike.Rego@baymetrics.com.br>
2025-06-24 14:09:23 -0300

    docs: correct wrong comment in code

--- src/back-end/validators/sign-up-validations.ts
+++ src/back-end/validators/sign-up-validations.ts
@@ -8,7 +8,7 @@ export default function normalizeEmail(email: string) {
   return `${userWithoutDots}@${domain}`;
 }
 
-//Função para verificar se o e-mai já existe
+//Função para verificar se o e-mail já existe
 export function emailAlreadyExists(email: string): boolean {
   const normalizedEmail = normalizeEmail(email);
   return mockUsers.some((user) => user.email === normalizedEmail);
