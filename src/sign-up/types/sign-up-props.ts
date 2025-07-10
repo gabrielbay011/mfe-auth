09859201c93ad464047307c52eda250c8e4ec55b
Mike Will <Mike.Rego@baymetrics.com.br>
2025-07-10 13:59:34 -0300

    style: modal style

--- src/sign-up/types/sign-up-props.ts
+++ src/sign-up/types/sign-up-props.ts
@@ -1,3 +1,3 @@
 export type SignUpProps = {
-    onSuccess: () => void;
-}
+  onSuccess: () => void;
+};
--- src/utils/components/modal.tsx
+++ src/utils/components/modal.tsx
@@ -6,10 +6,7 @@ export default function Modal({ isOpen, children }: ModalProps) {
 
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 w-70 md:w-100">
-      <div className="bg-white rounded-[20px] p-5 shadow-md">
-        {children}
-      </div>
+      <div className="bg-white rounded-[20px] p-5 shadow-md">{children}</div>
     </div>
   );
 }
-
